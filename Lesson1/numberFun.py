fab2df37aff05ac18b1c4fcb7c57fa2fbadfa05c
Stryker Wolf <skyrealmoblivion@gmail.com>
2021-01-19 19:39:13 -0500

    added final part of lesson

---
+++ Lesson1/numberFun.py
@@ -0,0 +1,28 @@
+import random
+import this
+number = random.randrange(1,100)
+number2 = random.randrange(1,100)
+print(f"{number} is the first number")
+print(f"{number2} is the second number")
+division = number/number2
+floor = number//number2
+modulus = number%number2
+addition = number+number2
+subtraction = number - number2
+multiplication = number*number2
+print(f"{number}/{number2}={division}")
+print(f"{number}//{number2}={floor}")
+print(f"{number}%{number2}={modulus}")
+print(f"{number}+{number2}={addition}")
+print(f"{number}-{number2}={subtraction}")
+print(f"{number}*{number2}={multiplication}")
+number2 = 25
+print(f"{number} is the first number")
+print(f"{number2} is the second number")
+print(f"{number}/{number2}={division}")
+print(f"{number}//{number2}={floor}")
+print(f"{number}%{number2}={modulus}")
+print(f"{number}+{number2}={addition}")
+print(f"{number}-{number2}={subtraction}")
+print(f"{number}*{number2}={multiplication}")
+print(this)
