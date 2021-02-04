1cd49fce6a335af5a8891c12758dae714d46ce89
Stryker Wolf <skyrealmoblivion@gmail.com>
2021-02-04 15:07:19 -0500

    fixed counter

--- Lesson7/mathMagic.py
+++ Lesson7/mathMagic.py
@@ -12,16 +12,21 @@ while counter<problems:
         if correctAnswer==yourAnswer:
             print("Great job, you answered correctly!")
             numberCorrect+=1
+            counter+=1
         else:
             print(f"Sorry, the correct answer is {correctAnswer}")
+            counter+=1
     if randOption==1:
         correctAnswer = int(randNumber1-randNumber2)
         yourAnswer = int(input(f"What is the interger value of {randNumber1}-{randNumber2}: "))
         if correctAnswer==yourAnswer:
             print("Great job, you answered correctly!")
             numberCorrect+=1
+            counter+=1
         else:
             print(f"Sorry, the correct answer is {correctAnswer}")
+            counter+=1
+            
 
 print("You answered", numberCorrect, " Questions correctly!")
 print("Thanks for playing.")
